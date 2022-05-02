1fa39911c346e7a498da03d1a5ba7bbbac1ab60c
user <user@mb033298.local>
2022-05-02 16:12:55 -0300

    remove unnecessary impl

--- MainStack.java
+++ MainStack.java
@@ -7,11 +7,6 @@ public class MainStack {
     
     public static void main(String[] args){
 
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
-        System.out.println(stack.top());
+        // Type your code here ...
     }
 }
